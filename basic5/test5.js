3994812385e92fa9c9dbd0b42eb291c36712c689
mukesh8521 <mukeshkumar8521.rr@gmail.com>
2025-04-18 04:52:51 +0000

    datatypes

---
+++ basic5/test5.js
@@ -0,0 +1,13 @@
+"use strict" // treat all js code as newer version
+
+//alert (3+3) // we are using node.js , not browser
+console.log(3+3)
+
+
+// bigint 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null)  // object 
+console.log(typeof undefined) // undefined
